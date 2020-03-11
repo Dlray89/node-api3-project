51f931b4a44783862cf9982045038fc04cfad84f
dlray89 <dlrayjr89@gmail.com>
2020-03-11 14:35:56 -0400

    Lets get started

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+//import server here
+const server = ("./server.js")
+
+const PORT = 5000
+
+server.listen(PORT, () => {
+    console.log(`\n ** The server is running on http://localhost: ${PORT} ** /n`)
+})
